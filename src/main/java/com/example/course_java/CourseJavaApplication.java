81c90d5cb9d228ec27dee50688daaaf69ad78a19
Sofie Pon <sonia10367@gmail.com>
2024-05-29 08:04:00 +0300

    First commit

---
+++ src/main/java/com/example/course_java/CourseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_java/CourseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
